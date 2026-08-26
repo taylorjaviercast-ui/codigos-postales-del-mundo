2bab89389abea7548da1f92a8b5516eda88f53da
taylorjaviercast-ui <taylorjaviercast@gmail.com>
2026-08-26 15:57:14 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -43,7 +43,7 @@
   /* ================================================================
      2. DATA LOADING
      ================================================================ */
-  const DATA_VERSION = '24';
+  const DATA_VERSION = '25';
 
   async function loadData() {
     if (DATA) return DATA;
